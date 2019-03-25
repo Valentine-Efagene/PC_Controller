22dc17e25e6201e6daca7732e056f40d9c88529f
Valentine-Efagene <efagenevalentine@gmail.com>
2019-03-25 05:13:06 +0100

    update

--- app/src/main/java/com/example/valentyne/pc_controller/MouseSketch.java
+++ app/src/main/java/com/example/valentyne/pc_controller/MouseSketch.java
@@ -2,28 +2,34 @@ package com.example.valentyne.pc_controller;
 
 import android.util.Log;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 import processing.core.PFont;
 import processing.core.PVector;
 
 public class MouseSketch extends PApplet {
     private static final String TAG = "MouseSketch";
-    int x, y;
-    MouseActivity activity;
-    int ANDROID_ALLOWANCE = 150;
-    PFont font;
-    int w, h;
-    Mouse mouse;
-    String screenSize;
-    int screenWidth;
-    int screenHeight;
+    private static final float SCROLL_MAX = 50;
+    private int x, y, scrollValue, scrollValueBuf;
+    private MouseActivity activity;
+    private int ANDROID_ALLOWANCE = 150;
+    private PFont font;
+    private int w, h;
+    private Mouse mouse;
+    private int screenWidth;
+    private int screenHeight;
     private Clicker left;
     private Clicker right;
+    private Clicker drag;
     private float lastClick = millis();
-    private boolean doubleClicked = false;
     private int clickerDefaultColor = color(213, 33, 323);
+    private boolean holding = false;
+    private ArrayList<String> keysPressed = new ArrayList<String>();
+    private Scroller scroller;
 
     public MouseSketch(MouseActivity activity) {
+        String screenSize;
         this.activity = activity;
         w = activity.getDisplayMetrics().widthPixels;
         h = activity.getDisplayMetrics().heightPixels;
@@ -85,9 +91,11 @@ public class MouseSketch extends PApplet {
     public void setup() {
         stroke(0);
         font = createFont("SansSerif", 40);
+        scroller = new Scroller((int)(height - 0.5 * ANDROID_ALLOWANCE) );
         mouse = new Mouse(width/2, height/2);
-        left = new Clicker(0, height - 2 * ANDROID_ALLOWANCE);
-        right = new Clicker(0, ANDROID_ALLOWANCE);
+        left = new Clicker(0, ANDROID_ALLOWANCE);
+        drag = new Clicker(width - ANDROID_ALLOWANCE, height - 2 * ANDROID_ALLOWANCE );
+        right = new Clicker(0, height - 2 * ANDROID_ALLOWANCE);
     }
 
     public void draw() {
@@ -96,21 +104,58 @@ public class MouseSketch extends PApplet {
         rectMode(CENTER);
         rect(width/2, height/2, width - 2 * ANDROID_ALLOWANCE, height - 2 * ANDROID_ALLOWANCE);
         rectMode(CORNER);
+
+        if(!mousePressed || (mousePressed && get(mouseX, mouseY) == scroller.getColor())){
+            if(!(scroller.getPosition().x == width/2)){
+                scrollValueBuf = (int)scroller.getPosition().x - width/2;
+
+                if(scrollValueBuf < 0){
+                    scrollValue = (int)map(scrollValueBuf, 0, -width/2, 0, -SCROLL_MAX);
+                }else{
+                    scrollValue = (int)map(scrollValueBuf, 0, width/2, 0, SCROLL_MAX);
+                }
+
+                activity.setAction(new Integer(scrollValue).toString());
+
+                scroller.setX(width/2);
+            }
+        }
+
+        if(!mousePressed) {
+            left.setColor(clickerDefaultColor);
+            drag.setColor(clickerDefaultColor);
+            right.setColor(clickerDefaultColor);
+        }
+
+        scroller.draw();
         mouse.draw();
         left.draw();
+        drag.draw();
         right.draw();
         x = (int)mouse.getPosition().y;
         y = width - (int)mouse.getPosition().x;
         activity.setX((int)map(x, ANDROID_ALLOWANCE, height - ANDROID_ALLOWANCE, 0, screenWidth));
         activity.setY((int)map(y, ANDROID_ALLOWANCE, width - ANDROID_ALLOWANCE, 0, screenHeight));
 
+        fill(0);
         textFont(font);
-        text(screenWidth + ", " + screenHeight, 100, ANDROID_ALLOWANCE + 50);
+        text(screenWidth + ", " + screenHeight, 50, 50);
+
+        for(int i = 0; i < keysPressed.size(); i++) {
+            if(keysPressed.get(i) == "C") {
+                text("Dragging", width - 2 * ANDROID_ALLOWANCE, 50);
+            }
+        }
     }
 
     public void mouseDragged() {
         if(mouseX < ANDROID_ALLOWANCE || mouseX > width - ANDROID_ALLOWANCE ||
                 mouseY < ANDROID_ALLOWANCE || mouseY > height - ANDROID_ALLOWANCE) {
+            if(get(mouseX, mouseY) == scroller.getColor()){
+                text(new Integer((int)map(mouseX - width/2, 0, 740, 0, SCROLL_MAX)).toString(), width - 2 * ANDROID_ALLOWANCE, 50);
+                scroller.setX(mouseX);
+            }
+
             return;
         }
 
@@ -120,6 +165,24 @@ public class MouseSketch extends PApplet {
     public void mouseReleased() {
         left.color = clickerDefaultColor;
         right.color = clickerDefaultColor;
+
+        if(mouseX < ANDROID_ALLOWANCE || mouseX > width - ANDROID_ALLOWANCE ||
+                mouseY < ANDROID_ALLOWANCE || mouseY > width - ANDROID_ALLOWANCE) {
+            Log.w(TAG, "Released");
+            if(mouseX > left.pos.x && mouseX < left.pos.x + left.w &&
+                    mouseY > left.pos.y && mouseY < left.pos.y + left.h) {
+                activity.setAction("C");
+                left.color = color(0, 0, 255);
+                Log.w(TAG, "Left Release");
+            }
+
+            if(mouseX > right.pos.x && mouseX < right.pos.x + right.w &&
+                    mouseY > right.pos.y && mouseY < right.pos.y + right.h) {
+                activity.setAction("D");
+                right.color = color(0, 0, 255);
+                Log.w(TAG, "Right Release");
+            }
+        }
     }
 
     public void mousePressed() {
@@ -128,6 +191,16 @@ public class MouseSketch extends PApplet {
             Log.w(TAG, "Pressed");
             if(mouseX > left.pos.x && mouseX < left.pos.x + left.w &&
                     mouseY > left.pos.y && mouseY < left.pos.y + left.h) {
+                if(keysPressed.contains("C")) {
+                    keysPressed.remove("C");
+                    activity.setAction("D");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 activity.setAction("A");
                 left.color = color(0, 0, 255);
                 Log.w(TAG, "Left click");
@@ -135,9 +208,37 @@ public class MouseSketch extends PApplet {
 
             if(mouseX > right.pos.x && mouseX < right.pos.x + right.w &&
                     mouseY > right.pos.y && mouseY < right.pos.y + right.h) {
+                if(keysPressed.contains("C")) {
+                    keysPressed.remove("C");
+                    activity.setAction("D");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 activity.setAction("B");
                 right.color = color(0, 0, 255);
                 Log.w(TAG, "Right click");
+            }
+
+            if(mouseX > drag.pos.x && mouseX < drag.pos.x + drag.w &&
+                    mouseY > drag.pos.y && mouseY < drag.pos.y + drag.h) {
+                drag.color = right.color = color(0, 0, 255);;
+                holding = !holding;
+
+                if(holding) {
+                    if(!keysPressed.contains("C")) {
+                        keysPressed.add("C");
+                        activity.setAction("C");
+                    }
+                }else{
+                    if(keysPressed.contains("C")) {
+                        keysPressed.remove("C");
+                        activity.setAction("D");
+                    }
+                }
             }
 
             /*if(doubleClick()) {
@@ -230,6 +331,44 @@ public class MouseSketch extends PApplet {
 
         public PVector getPosition(){
             return position;
+        }
+    }
+
+    public class Scroller {
+        private PVector position;
+        final int DIAMETER = 100;
+        private int colour;
+
+        public int getColor() {
+            return colour;
+        }
+
+        public Scroller(int y){
+            position = new PVector();
+            this.position.x = width/2;
+            this.position.y = y;
+            colour = color(32, 3, 32);
+        }
+
+        public void draw(){
+            fill(color(23, 32, 54));
+            rectMode(CENTER);
+            rect(width/2, position.y, width, 10);
+            rectMode(CORNER);
+            fill(colour);
+            ellipse(position.x, position.y, DIAMETER, DIAMETER);
+        }
+
+        public PVector getPosition(){
+            return position;
+        }
+
+        public void setX(int x){
+            position.x = x;
+        }
+
+        public void setY(int y) {
+            position.y = y;
         }
     }
 }
